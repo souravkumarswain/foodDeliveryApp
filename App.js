79a0592ae34e9a2941d5c05197423fb2b80a0c51
souravkumar <jewelsourav@gmail.com>
2026-05-25 18:31:02 +0530

    initial set up for the food delivery app

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const DeliveryAppLayout = () => {
+    return <>
+        <h1>Food Delivery App</h1>
+        <p>Welcome to the Food Delivery App</p>
+    </>
+}
+
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<DeliveryAppLayout/>)
