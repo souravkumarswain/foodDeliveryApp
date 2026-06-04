c50e4ca42f77350d56c5cec19bfe931dd3b21d8e
sourav kumar swain <souravkumarswain96@gmail.com>
2026-06-04 12:39:13 +0530

    Filters added to UI

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,16 +1,40 @@
+import { useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
+import mockData from "../utils/mockData";
+
 
 const Body = () => {
-    return <>
-    <input className= "search-bar" type="text" placeholder="Search your Food.."></input>
-    <div className="restaurant-list">
-        {
-            resList.map((restaurant) => {
-                return <RestaurantCard key={restaurant.info.id} restaurant={restaurant}/>
-            })
+    const [resList, setResList] = useState(mockData);
+    const handleChange = (event) => {
+        if (event.target.value === "best_rated") {
+            let filteredList = mockData.filter((res) => res.info.avgRating >= 4);
+            setResList(filteredList);
+        } else {
+            setResList(mockData);
         }
-    </div>
+    };
+
+    return <>
+        <div className="search-and-filter">
+            <input className="search-bar" type="text" placeholder="Search your Food.."></input>
+
+            <div className="filter">
+                <label htmlFor="sorting-filter">Sort By:</label>
+                <select id="sorting-filter" onChange={handleChange}>
+                    <option value="all">All Restaurants</option>
+                    <option value="best_rated">Best Rated</option>
+                </select>
+            </div>
+        </div>
+
+        <div className="restaurant-list">
+            {
+                resList.map((restaurant) => {
+                    return <RestaurantCard key={restaurant.info.id} restaurant={restaurant} />
+                })
+            }
+        </div>
     </>
 }
 
