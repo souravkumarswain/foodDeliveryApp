6daab074e8a5f84ddfe9c9d6b7bda17d936ff393
sourav kumar swain <souravkumarswain96@gmail.com>
2026-06-04 21:56:05 +0530

    dynamic name change login btn to logout using usestate

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 import {LOGO_URL} from "../utils/constants";
+import {useState} from "react";
 
 const Header = () =>{
+    const [btnReact,setBtnReact] = useState("Login");
     return <>
         <div className = "header">
             <div className="logo">
@@ -10,6 +12,9 @@ const Header = () =>{
                 <li>Home</li>
                 <li>About</li>
                 <li>Contact</li>
+                <button className = "login-button" onClick= {()=> {
+                    btnReact === "Login" ? setBtnReact("Logout") : setBtnReact("Login")
+                }}>{btnReact}</button>
             </ul>
         </div>
     </>
