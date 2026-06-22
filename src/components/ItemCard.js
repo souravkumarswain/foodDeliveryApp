7cb55b651bee801df692577dfc7129151eea0c3f
souravkumar <jewelsourav@gmail.com>
2026-06-22 13:14:05 +0530

    Higher order function added to Item card

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -2,8 +2,7 @@ import { VEG_LOGO, NON_VEG_LOGO, RATING_STAR } from "../utils/constants";
 import Shimmer from "./Shimmer";
 
 const ItemCard = (props) => {
-    // console.log(props)
-    const {product_name, small_description, rating, price,is_veg,image_es} = props.items;
+    const { product_name, small_description, rating, price, is_veg, image_es } = props.items;
 
     // const products = Array.isArray(props?.items?.products) ? props.items.products : [];
 
@@ -11,33 +10,38 @@ const ItemCard = (props) => {
     //     return <Shimmer />;
     // }
 
-    return(
-        <>
-        <div>
-            <div></div>
-            <div className="w-60 m-4 p-4 border border-gray-300 rounded-md shadow-md hover:hover:scale-105 transition-transform duration-300 cursor-pointer">
+    return (      
             <div>
-                <img className="w-50 h-48 object-cover mb-4" src={image_es} alt="ItemImg"/>
-            </div>
-            <div className="h-46">
-                <h1 className="font-bold text-xl items-baseline">{is_veg ? <img src={VEG_LOGO} className="inline-block mr-2 h-4 mb-0.5" alt="Veg" /> : <img src={NON_VEG_LOGO} className="inline-block mr-2 h-4" alt="Non-Veg" />}{product_name.length > 28 ? product_name.slice(0, 28) + "..." : product_name}</h1>
-                <p className="text-gray-400 h-20">{small_description.length > 70 ? small_description.slice(0, 70)+"...":small_description}</p>
-                <span className="flex justify-between items-center mt-2 mb-2">
-                    <p className=" text-xs text-green-800 font-bold items-center align-center bg-green-100 w-16 h-8 p-2"><img src={RATING_STAR} className="inline-block mr-2 h-3 mb-0.5" alt="Rating" />{rating} </p>
-                </span>   
-            </div>
-            <hr className="my-2 -ml-4 -mr-4 opacity-10"/>
-            <div className="flex justify-between items-center h-10">
-                <p className="font-bold text-lg">₹ {price}</p>
-                <button className="bg-transparent border border-indigo-500 text-indigo-500 p-2 rounded-md hover:border-green-500 hover:text-green-500 w-24">Add</button>
-            </div>
+                <div className="w-60 m-4 p-4 border border-gray-300 rounded-md shadow-md hover:hover:scale-105 transition-transform duration-300 cursor-pointer">
+                    <div>
+                        <img className="w-50 h-48 object-cover mb-4" src={image_es} alt="ItemImg" />
+                    </div>
+                    <div className="h-46">
+                        <h1 className="font-bold text-xl items-baseline">{is_veg ? <img src={VEG_LOGO} className="inline-block mr-2 h-4 mb-0.5" alt="Veg" /> : <img src={NON_VEG_LOGO} className="inline-block mr-2 h-4" alt="Non-Veg" />}{product_name.length > 28 ? product_name.slice(0, 28) + "..." : product_name}</h1>
+                        <p className="text-gray-400 h-20">{small_description.length > 70 ? small_description.slice(0, 70) + "..." : small_description}</p>
+                        <span className="flex justify-between items-center mt-2 mb-2">
+                            <p className=" text-xs text-green-800 font-bold items-center align-center bg-green-100 w-16 h-8 p-2"><img src={RATING_STAR} className="inline-block mr-2 h-3 mb-0.5" alt="Rating" />{rating} </p>
+                        </span>
+                    </div>
+                    <hr className="my-2 -ml-4 -mr-4 opacity-10" />
+                    <div className="flex justify-between items-center h-10">
+                        <p className="font-bold text-lg">₹ {price}</p>
+                        <button className="bg-transparent border border-indigo-500 text-indigo-500 p-2 rounded-md hover:border-green-500 hover:text-green-500 w-24">Add</button>
+                    </div>
 
-           </div>
-        </div>
-           
-        </>
+                </div>
+            </div>
     )
 }
-
+export const itemCardWithPromo = (ItemCard) => {
+    return (props) => {
+        return (
+            <div className="relative">
+                <p className="absolute text-white text-sm font-bold left-4 top-4 bg-gradient-to-b from-green-300 to-green-600 rounded-lg p-1">BUY1GET1</p>
+                <ItemCard {...props} />
+            </div>
+        )
+    }
+}
 
 export default ItemCard;
--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,8 +1,9 @@
 import ItemCard from "./ItemCard";
 import { useState } from "react";
+import { itemCardWithPromo } from "./ItemCard";
 
 const RestaurantCategory = (props) => {
-    const {category,showItems, setShowIndex} = props;
+    const { category, showItems, setShowIndex } = props;
 
     const [isCategoryOpen, setCategoryOpen] = useState(true)
 
@@ -11,17 +12,20 @@ const RestaurantCategory = (props) => {
         setShowIndex();
     }
 
+    const PromotedItems = itemCardWithPromo(ItemCard)
+
     return (
-        <>   
+        <>
             <div className="flex">
-                <div onClick= {onClickCategory} className="w-160 font-bold mb-3 text-gray-700 text-lg hover:border-l-4 p-2 border-blue-600 cursor-pointer">
+                <div onClick={onClickCategory} className="w-160 font-bold mb-3 text-gray-700 text-lg hover:border-l-4 p-2 border-blue-600 cursor-pointer">
                     <span>
                         <h2 className="p-4">{category.collection_name} ({category.products.length})</h2>
                     </span>
                 </div>
 
                 <div className="flex flex-wrap gap-4 p-4">
-                    {isCategoryOpen && showItems ? category.products.map((item) => { return <ItemCard key={item.product_id} items={item} /> }) : null}
+                    {isCategoryOpen && showItems ? category.products.map((item) =>  item.offer_tags[0] === 'BUY_1_GET_1' ? (<PromotedItems key={item.product_id} items={item} />) : (<ItemCard key={item.product_id} items={item} />) 
+                    ) : null}
                 </div>
             </div>
         </>
